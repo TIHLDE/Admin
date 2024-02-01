cdad65a69aedd251ad815497664d7782c356faac
Mads Nylund <madsnyl@gmail.com>
2024-02-01 20:40:51 +0100

    fixed export bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/sonner";
 import Navbar from "./nav";
 import { cn } from "@/lib/utils";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
